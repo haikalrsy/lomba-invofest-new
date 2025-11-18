66b48a2b1a4b20ae04accad0a8af3cc828790a6f
haikalrsy <haikalrasyaputra@gmail.com>
2025-11-19 00:13:18 +0700

    routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+   base: '/lomba-invofest-new/',
   plugins: [react()],
   resolve: {
     alias: {

3f1ec68a3b4d7664016acfa070ee7c3181b1585e
haikalrsy <haikalrasyaputra@gmail.com>
2025-11-19 00:19:08 +0700

    route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/lomba-invofest-new/', // wajib untuk GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
